11a2b4e01fb014f5c84c4c11c8843567bd2c387f
“adash333” <adash333@hotmail.com>
2021-10-09 17:19:08 +0900

    first commit

--- RockPaperScissors WatchKit Extension/ContentView.swift
+++ RockPaperScissors WatchKit Extension/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // じゃんけんの結果を格納する変数
+    // （0=初期画面、1=グー、2=チョキ、3=パー）
+    @State var answerNumber = 0
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            if answerNumber == 0 {
+                Image(/*@START_MENU_TOKEN@*/"janken_boys"/*@END_MENU_TOKEN@*/)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else if answerNumber == 1 {
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("グー")
+            } else if answerNumber == 2 {
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("チョキ")
+            } else {
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("パー")
+            }
+            
+            Button(action: {
+                print("タップされたよ")
+                answerNumber = Int.random(in: 1...3)
+            }) {
+                /*@START_MENU_TOKEN@*/Text("じゃんけんをする！")/*@END_MENU_TOKEN@*/
+            }
+            
+        }
     }
 }
 
