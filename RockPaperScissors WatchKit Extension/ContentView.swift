63bdd4215a76fc4d4b22d547365125f56c2d9a22
“adash333” <adash333@hotmail.com>
2021-10-09 12:42:33 +0900

    Initial Commit

---
+++ RockPaperScissors WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors WatchKit Extension
+//
+//  Created by a on 2021/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors WatchKit Extension/RockPaperScissorsApp.swift
@@ -0,0 +1,19 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors WatchKit Extension
+//
+//  Created by a on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
